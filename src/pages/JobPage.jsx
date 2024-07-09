175d865bc9d3d0e86f7aafbfe0a20e3f16ea517d
Abdi Kumela <abdikumela036@gmail.com>
2024-07-09 16:07:07 +0300

    fix a bug in joppage

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { FaMapMarker } from "react-icons/fa";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 
 export default function JobPage() {
   const [job, setJob] = useState(null);
@@ -32,38 +32,75 @@ export default function JobPage() {
   }
 
   return (
-    <div className="container mx-auto p-4">
-      {job ? (
-        <div className="bg-white rounded-xl shadow-md p-6 m-5">
+    <>
+      <section>
+        <div className="container mx-auto py-6 px-6">
           <Link
-            to={`/`}
-            className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+            to="/"
+            className="text-indigo-500 hover:text-indigo-600 flex items-center"
           >
-            Back
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
           </Link>
-          <div className="mb-6">
-            <div className="text-gray-600 my-2">{job.type}</div>
-            <h3 className="text-2xl font-bold">{job.title}</h3>
-          </div>
-          <div className="mb-5">{job.description}</div>
-          <h3 className="text-indigo-500 mb-2">{job.salary}/ Year</h3>
-          <div className="border border-gray-100 mb-5"></div>
-          <div className="flex flex-col lg:flex-row justify-between mb-4">
-            <div className="text-orange-700 mb-3 lg:mb-0">
-              <FaMapMarker className="inline" />
-              {job.location}
-            </div>
-            <Link
-              to={`/job/${job.id}`}
-              className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-            >
-              Apply Now
-            </Link>
+        </div>
+      </section>
+
+      <section className="bg-indigo-50 min-h-screen">
+        <div className="container mx-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-[70%,30%] w-full gap-6">
+            <main>
+              <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                <div className="text-gray-500 mb-4">{job.type}</div>
+                <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
+                <div className="text-gray-500 mb-4 flex items-center justify-center md:justify-start">
+                  <FaMapMarker className="text-orange-700 text-lg mr-2" />
+                  <p className="text-orange-700">{job.location}</p>
+                </div>
+              </div>
+
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-indigo-800 text-lg font-bold mb-6">
+                  Description
+                </h3>
+                <p className="mb-4">{job.description}</p>
+                <h3 className="text-indigo-800 text-lg font-bold mb-2">
+                  Salary
+                </h3>
+                <p className="mb-4">{job.salary} / Year</p>
+              </div>
+            </main>
+
+            <aside>
+              <div className="bg-white p-6 rounded-lg shadow-md">
+                <h3 className="text-xl font-bold mb-6">Company Info</h3>
+                <h2 className="text-2xl">{job.company.name}</h2>
+                <p className="my-2">{job.company.description}</p>
+                <hr className="my-4" />
+                <h3 className="text-xl">Contact Email:</h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactEmail}
+                </p>
+                <h3 className="text-xl">Contact Phone:</h3>
+                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                  {job.company.contactPhone}
+                </p>
+              </div>
+
+              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+                <Link
+                  to={`/edit-job/${job.id}`}
+                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Edit Job
+                </Link>
+                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                  Delete Job
+                </button>
+              </div>
+            </aside>
           </div>
         </div>
-      ) : (
-        <div>Job not found.</div>
-      )}
-    </div>
+      </section>
+    </>
   );
 }
